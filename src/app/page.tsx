5f938901ef0ea451ff6075e9c8992625369764fd
hayama <hayamaroom@gmail.com>
2024-02-05 15:53:44 +0800

    Add login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,23 @@
 import { ModeToggle } from "@/components/mode-toggle";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { LogInIcon } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <Button>Click Me</Button>
       <ModeToggle />
+      <Link href={'/login'}
+        className={cn(
+          'space-x-2',
+          buttonVariants()
+        )}
+      >
+        <LogInIcon className="h-4 w-4" />
+        <span>Login</span>
+      </Link>
     </div>
   );
 }
