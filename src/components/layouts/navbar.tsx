e0d80fa1eeeed479080ce7c8ac05b1633c9204b7
hayama <hayamaroom@gmail.com>
2024-02-06 15:22:45 +0800

    Add sidebar

--- src/components/layouts/navbar.tsx
+++ src/components/layouts/navbar.tsx
@@ -2,14 +2,20 @@ import React from 'react'
 import { navbarMenu } from '@/lib/menu'
 import Link from 'next/link'
 import { ModeToggle } from '../mode-toggle'
+import { cn } from '@/lib/utils'
+import { buttonVariants } from '../ui/button'
+import Logo from './logo'
+import Sidebar from './sidebar'
 
 export default function Navbar() {
   return (
     <div className='border-b border-border bg-background/90 shadow-md'>
       <div className='container mx-auto drop-shadow-md'>
         <div className='flex items-center justify-between h-14'>
-          <div className='flex-none text-2xl font-roboto font-extrabold'>LOGO</div>
-          <div className='flex justify-between space-x-2'>
+          <div className='flex-none '>
+            <Logo />
+          </div>
+          <div className='hidden sm:flex justify-between space-x-2'>
             {navbarMenu.map((m, i) => (
               <Link key={i} href={m.path}
                 className=''
@@ -18,8 +24,9 @@ export default function Navbar() {
               </Link>
             ))}
           </div>
-          <div>
+          <div className='flex items-center space-x-2'>
             <ModeToggle />
+            <Sidebar />
           </div>
         </div>
       </div>
