67c466c6225a3de089e17e28a4e9390e155697d9
hayama <hayamaroom@gmail.com>
2024-02-06 11:20:50 +0800

    Add navbar menu

--- src/components/layouts/navbar.tsx
+++ src/components/layouts/navbar.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import { navbarMenu } from '@/lib/menu'
+import Link from 'next/link'
 
 
 export default function Navbar() {
@@ -7,7 +8,11 @@ export default function Navbar() {
     <div className='flex'>
       <div>LOGO</div>
       <div className='flex'>
-        
+        {navbarMenu.map((m, i) => (
+          <Link key={i} href={m.path}>
+            {m.title}
+          </Link>
+        ))}
       </div>
     </div>
   )
