b03b3b7b25d331eb1d57113d881ecfee6e40d0ca
hayama <hayamaroom@gmail.com>
2024-02-07 16:38:24 +0800

    Change footer page

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className='container mx-auto p-10 md:flex items-center justify-between text-gray-600 space-y-4'>
-      <div className='font-bold'>
-        LOGO
-      </div>
-      <div className='text-lg'>
-        Lama creative thoughts agency © All rights reserved.
+    <div className='border-t border-border mt-10'>
+      <div className='container mx-auto p-10 md:flex items-center justify-between text-gray-600 space-y-4'>
+        <div className='font-bold'>
+          LOGO
+        </div>
+        <div className='text-lg'>
+          Lama creative thoughts agency © All rights reserved.
+        </div>
       </div>
     </div>
   )
