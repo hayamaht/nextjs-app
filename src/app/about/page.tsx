76dd8312ed486b3f513d22c02f9fe7a84bd3c50a
hayama <hayamaroom@gmail.com>
2024-02-07 11:11:11 +0800

    Change about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,10 +9,10 @@ export const metadata:Metadata = {
 
 export default function AboutPage() {
   return (
-    <div className='container mx-auto p-10 sm:flex gap-10'>
+    <div className='container mx-auto p-10 sm:flex gap-10 space-y-10 sm:space-y-0'>
       <div className='flex-1 flex flex-col gap-5'>
-        <h2 className='text-3xl font-bold'>About Agency</h2>
-        <h3 className='text-green-500 text-2xl'>We create digital ideas that are bigger, bolder, braver and better.</h3>
+        <h2 className='text-xl font-bold text-primary'>About Agency</h2>
+        <h3 className='text-4xl font-black'>We create digital ideas that are bigger, bolder, braver and better.</h3>
         <p className='text-lg'>We create digital ideas that are bigger, bolder, braver and better. We
           believe in good ideas flexibility and precission We’re world’s Our
           Special Team best consulting & finance solution provider. Wide range
@@ -20,15 +20,15 @@ export default function AboutPage() {
         </p>
         <div className='flex items-center justify-between'>
           <div className='flex flex-col gap-4'>
-            <h2 className='text-primary'>10 K+</h2>
+            <h2 className='text-primary font-black text-2xl'>10 K+</h2>
             <p>Year of experience</p>
           </div>
           <div className='flex flex-col gap-4'>
-            <h3 className='text-primary'>10 K+</h3>
+            <h3 className='text-primary font-black text-2xl'>10 K+</h3>
             <p>Year of experience</p>
           </div>
           <div className='flex flex-col gap-4'>
-            <h3 className='text-primary'>10 K+</h3>
+            <h3 className='text-primary font-black text-2xlK'>10 K+</h3>
             <p>Year of experience</p>
           </div>
         </div>
