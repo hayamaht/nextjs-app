b82cf11413ae0c0f5eba82f045082eb0cc048650
hayama <hayamaroom@gmail.com>
2024-02-07 10:59:15 +0800

    Add footer page

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='container mx-auto p-10 md:flex items-center justify-between text-gray-600 space-y-4'>
+      <div className='font-bold'>
+        LOGO
+      </div>
+      <div className='text-lg'>
+        Lama creative thoughts agency © All rights reserved.
+      </div>
+    </div>
   )
 }
